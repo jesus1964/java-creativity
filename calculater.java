92db0be456235ce6782c5b855879fac25713438f
Antony vinnarasi <69240388+jesus1964@users.noreply.github.com>
2021-07-20 21:17:19 +0530

    Add files via upload

---
+++ calculater.java
@@ -0,0 +1,11 @@
+public class calculater{
+    public static void main(String args[]){
+        calculater j=new calculater();
+        j.add(3,5);
+    }
+    void add(int a,int b){
+        int c=a+b;
+        System.out.println(c);
+
+    }
+}
